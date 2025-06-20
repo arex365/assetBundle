05fcca83b62bb6f3f0b174ff4a6ba29e5ac31fc4
arex365 <muhammad.owais@hitostudios.net>
2025-06-20 14:38:14 +0500

    added crossplatform

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware to serve platform-specific static files
+app.use((req, res, next) => {
+  const platform = req.headers['platform'];
+
+  let platformDir;
+  if (platform === 'pc') {
+    platformDir = 'pc';
+  } else if (platform === 'android') {
+    platformDir = 'android';
+  } else {
+    return res.status(400).send('Missing or invalid platform header');
+  }
+
+  express.static(path.join(__dirname, platformDir))(req, res, next);
+});
 
 app.get('/', (req, res) => {
   res.send('AssetBundle Server Running');
